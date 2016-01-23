33cdbc75c77812b498e117a488319166ee89870c
Najm Sheikh <najm97@gmail.com>
2016-01-23 04:28:00 -0500

    Added base FB chat bot API

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var bodyParser = require('facebook-chat-api');
+var login = require('facebook-chat-api');
+
 var port = process.env.PORT || 1337;
+var fbuser = process.env.QNS_FBUSER;
+var fbpass = process.env.QNS_PASS;
 
 var app = express();
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
+login({
+    email: fbuser,
+    password: fbpass
+}, function callback(err, api) {
+    if (err) return console.error(err);
+
+    api.listen(function callback(err, message) {
+        if (message.body.includes("@qbot"))
+            api.sendMessage(message.body, message.threadID);
+    });
+});
+
+app.listen(port, function() {
+    console.log("Server is up  running at port: " + port);
+});
